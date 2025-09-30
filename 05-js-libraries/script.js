184e931e418c22a9174c273848d64d40af626426
rurvur <rhys.cabot@gmail.com>
2025-09-29 23:46:27 -0400

    The first push

--- 05-js-libraries/script.js
+++ 05-js-libraries/script.js
@@ -1,3 +1,53 @@
+//onclick events to get the notifications to show!
+window.onload = function () {
+    const crumble = document.getElementById("cuberry");
+    const hearts = document.getElementById("heartbeet");
+    const meats = document.getElementById("spikyhen");
+
+
+    crumble.addEventListener("click", () => {
+      Toastify({
+        text: "Contains: fruit. Do not allow Tabby slimes to partake!",
+        duration: 3000,
+        gravity: "top",  
+        position: "right", //Positions the notifications at the top right, unintrusive but noticeable
+        style: {
+            background: "linear-gradient(to right, #d6845cff, #c86bb2ff)",
+        }
+      }).showToast();
+    });
+
+    hearts.addEventListener("click", () => {
+      Toastify({
+        text: "Contains: vegetables. Carnivorous and fruit-loving slimes may find this too bitter!",
+        duration: 3500,
+        gravity: "top",  
+        position: "right",
+        style: {
+            background: "linear-gradient(to right, #526fbdff, #5ab29cff)",
+        }
+      }).showToast();
+    });
+
+    meats.addEventListener("click", () => {
+      Toastify({
+        text: "Contains: meat. Careful with your vegetarian slimes! This happens to be perfect for your carnivores though",
+        duration: 4000,
+        gravity: "top",  
+        position: "right",
+        style: {
+            background: "linear-gradient(to right, #350000ff, #000000ff)",
+        }
+      }).showToast();
+    });
+  };
+
+
+
+
+
+
+//This stuff is just notes from the class demo, used for reference.
 class Person {
     constructor(n) {
         this.name = n;
