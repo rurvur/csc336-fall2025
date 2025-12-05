9f300fbe42379e97e1026ada50a2974dcaa2c102
rurvur <rhys.cabot@gmail.com>
2025-12-05 12:01:39 -0500

    Some slight fixups

--- final/README.md
+++ final/README.md
@@ -1,3 +1,3 @@
 # Rhys's Final Website: The Pokemon Team Organizer!
 
-Here is the [link](https://www.google.com).
+Here is the [link](https://csc336-fall2025-cbpt.onrender.com).
--- final/client/src/Pokemon1.jsx
+++ final/client/src/Pokemon1.jsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from "react";
 
 function Pokemon1() {
-  const [fromServer, setFromServer] = useState({something: 0});
+  const [fromServer, setFromServer] = useState({something: "Name"});
 
   useEffect(() => {
     fetch("http://localhost:3000/api/pokemon")
       .then(res => res.json())
-      .then(data => setFromServer(data));
+      .then(data => setFromServer({something:data.pokemon.team[0].name}));
   }, []);
 
   return (
     <>
       <div id = "slot1body">
-        <h1 id = "name">Name</h1>
+        <h1 id = "name">{fromServer.something}</h1>
         <h2 id = "nickname">Nickname</h2>
         <hr></hr>
         <div id = "description">This is where the description would go... IF I HAD ONE</div>
@@ -20,7 +20,6 @@ function Pokemon1() {
         <div id = "moves">Here is where the moves would go if you added them properly</div>
         <div id = "stats">Here is where the stats would go</div>
         <div id = "details">This is where the rest of the stuff goes</div>
-        <h1>{fromServer.something}</h1>
       </div>
     </>
   )
